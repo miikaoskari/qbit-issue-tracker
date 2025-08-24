ed1355097575945b0dd8472d5196d6bacb07a2dc
miikaoskari <miika.karjalainen13@gmail.com>
2025-08-24 18:42:22 +0300

    isort plus formatting

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import qbittorrentapi
 import json
-import schedule
-import time
 import logging
+import time
+
+import qbittorrentapi
+import schedule
 
 
 class QbitIssueHandler:
@@ -11,7 +12,7 @@ class QbitIssueHandler:
         logging.basicConfig(
             filename="qbit-issue-tracker.log",
             level=logging.INFO,
-            format="%(asctime)s %(levelname)s: %(message)s"
+            format="%(asctime)s %(levelname)s: %(message)s",
         )
 
     def tag_torrents_with_issues(self):
@@ -24,14 +25,18 @@ class QbitIssueHandler:
                 elif tracker.status == 2 and "issue" in torrent.tags:
                     # Tracker is working again so remove the issue tag
                     torrent.remove_tags("issue")
-                    logging.info(f"removed issue tag from {torrent.name} - {torrent.hash}")
+                    logging.info(
+                        f"removed issue tag from {torrent.name} - {torrent.hash}"
+                    )
 
     def qbit_logout(self):
         self.qbt_client.auth_log_out()
 
+
 def get_login_data() -> dict:
     with open("login.json") as f:
         return json.loads(f.read())
+
 
 def job():
     conn_info = get_login_data()
@@ -40,13 +45,15 @@ def job():
     ih.tag_torrents_with_issues()
     ih.qbit_logout()
 
+
 def main():
     job()
 
     schedule.every(24).hours.do(job)
     while True:
         schedule.run_pending()
-        time.sleep(60*60) # check every hour
+        time.sleep(60 * 60)  # check every hour
+
 
 if __name__ == "__main__":
     main()
